5f365155d189333eb4b52de132515126a476ef6c
a-gallina <andreagallina.ga@gmail.com>
2026-08-31 21:14:25 +0200

    Completato codice per la lettura file FASTA

---
+++ fasta_io.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+char *read_fasta(const char *filename){
+    FILE *file = fopen(filename, "r"); //apertura file in lettura
+
+    if(file == NULL){
+        printf("Errore lettura file");
+        return NULL;
+    }
+
+    char buffer[256]; //legge una riga
+    char sequence[1000] = "";
+
+    while(fgets(buffer, sizeof(buffer), file) != NULL){
+
+        buffer[strcspn(buffer, "\n")] = 0;
+
+        if(buffer[0] == '>') { //se la riga e' una intestazione(inizia con >)
+            printf("Trovata intestazione: %s\n", buffer);
+
+        }
+        else{
+            printf("Letta sequenza: %s\n", buffer);
+            strcat(sequence, buffer);
+        }
+    }
+
+    char *result = malloc(strlen(sequence) + 1);
+    strcpy(result, sequence);
+
+    fclose(file);
+    return result;
+}
---
+++ fasta_io.h
@@ -0,0 +1,6 @@
+#ifndef FASTA_IO_H
+#define FASTA_IO_H
+
+char *read_fasta(const char *filename);
+
+#endif
